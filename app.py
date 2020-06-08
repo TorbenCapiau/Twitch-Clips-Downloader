1fab090c88fd7cebe034490c38f5a52ca171e4a8
Torben <torben.capiau@accenture.com>
2020-06-08 18:29:28 +0200

    Paging based on Twitch API, supports more URL schemes

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
-import requests, argparse, base64, os, unicodedata
+import requests, argparse, os, unicodedata
 from slugify import slugify
 
-download_path = "./test"
+download_path = "./downloads"
 
 def GetCuratorId(username, auth):
     data = [{"operationName":"ClipsManagerTable_User","variables":{"login":username,"limit":20,"criteria":{"sort":"CREATED_AT_DESC","period":"ALL_TIME","curatorID":"0"},"cursor":None},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"b300f79444fdcf2a1a76c101f466c8c9d7bee49b643a4d7878310a4e03944232"}}}]
     r = requests.post("https://gql.twitch.tv/gql", headers={"Authorization": "OAuth {}".format(auth), "Client-Id": "kimne78kx3ncx6brgo4mv6wki5h1ko"}, json=data)
     return r.json()[0]['data']['user']['id']
 
-def DownloadClip(clip, filename):
-    filename = "{}.mp4".format(slugify(filename))
+def DownloadClip(clip, filename, i):
+    filename = "{}-{}.mp4".format(str(i), slugify(filename))
     if "AT-cm" not in clip['node']['thumbnailURL']:
-        if "offset" in clip['node']['thumbnailURL']:
+        if "offset" in clip['node']['thumbnailURL'] or "index" in clip['node']['thumbnailURL']:
             video_id = clip['node']['thumbnailURL'].split('/')[3].split('-')[0]
             offset = clip['node']['thumbnailURL'].split('/')[3].split('-')[2]
-            clip_url = "https://{}/{}-offset-{}.mp4".format(clip['node']['thumbnailURL'].split('/')[2], str(video_id), str(offset))
+            if len(clip['node']['thumbnailURL'].split('/')[3].split('-')) > 5:
+                if "vod" in clip['node']['thumbnailURL'].split('/')[3]:
+                    clip_url = "https://{}/vod-{}-offset-{}.mp4".format(clip['node']['thumbnailURL'].split('/')[2], clip['node']['thumbnailURL'].split('/')[3].split('-')[1], clip['node']['thumbnailURL'].split('/')[3].split('-')[3])
+                else:
+                    clip_url = "https://{}/{}-offset-{}-{}.mp4".format(clip['node']['thumbnailURL'].split('/')[2], str(video_id), clip['node']['thumbnailURL'].split('/')[3].split('-')[2], clip['node']['thumbnailURL'].split('/')[3].split('-')[3])
+            else:
+                clip_url = "https://{}/{}-offset-{}.mp4".format(clip['node']['thumbnailURL'].split('/')[2], str(video_id), str(offset))
         else:
             clip_url = "https://{}/{}.mp4".format(clip['node']['thumbnailURL'].split('/')[2], clip['node']['thumbnailURL'].split('/')[3].split('-')[0])
     else:
         if "AT-cm" in clip['node']['thumbnailURL']:
             clip_url = "https://{}/AT-{}.mp4".format(clip['node']['thumbnailURL'].split('/')[2], clip['node']['thumbnailURL'].split('/')[3].split('-')[1])
+    if "-index-" in clip['node']['thumbnailURL']:
+        clip_url = clip_url.replace("-offset-", "-index-")
     r = requests.get(clip_url)
     with open("{}/{}".format(download_path, filename), 'wb') as f:
         f.write(r.content)
@@ -50,6 +58,7 @@ curatorId = GetCuratorId(Twitch_Username, Twitch_Auth)
 limit_increase = 20
 currentCount = 0
 cursor = None
+i = 0
 
 print("[SUCCESS] Authenticated, curator ID: {}".format(curatorId))
 data = [{"operationName":"ClipsManagerTable_User","variables":{"login":Twitch_Username,"limit":limit_increase,"criteria":{"sort":"CREATED_AT_DESC","period":"ALL_TIME","curatorID":curatorId},"cursor":cursor},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"b300f79444fdcf2a1a76c101f466c8c9d7bee49b643a4d7878310a4e03944232"}}}]
@@ -57,12 +66,13 @@ data = [{"operationName":"ClipsManagerTable_User","variables":{"login":Twitch_Us
 while not doneParsing:
     r = requests.post("https://gql.twitch.tv/gql", headers={"Authorization": "OAuth {}".format(Twitch_Auth), "Client-Id": "kimne78kx3ncx6brgo4mv6wki5h1ko"}, json=data)
     currentCount = currentCount + limit_increase
-    cursor = base64.b64encode(str(currentCount).encode('ascii')).decode("utf-8", "ignore")
     clips = r.json()[0]['data']['user']['clips']['edges']
+    cursor = clips[len(clips) - 1]['cursor']
     data = [{"operationName":"ClipsManagerTable_User","variables":{"login":Twitch_Username,"limit":limit_increase,"criteria":{"sort":"CREATED_AT_DESC","period":"ALL_TIME","curatorID":curatorId},"cursor":str(cursor)},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"b300f79444fdcf2a1a76c101f466c8c9d7bee49b643a4d7878310a4e03944232"}}}]
     for clip in clips:
+        i = i + 1
         print("[INFO] Found clip: {} on {} ({})".format(clip['node']['title'], clip['node']['broadcaster']['displayName'], clip['node']['url']))
-        DownloadClip(clip, "{} - {} - {}".format(clip['node']['createdAt'].split('T')[0], clip['node']['broadcaster']['displayName'], clip['node']['title']))
-    if len(clips) < limit_increase:
+        DownloadClip(clip, "{} - {} - {}".format(clip['node']['createdAt'].split('T')[0], clip['node']['broadcaster']['displayName'], clip['node']['title']), i)
+    if not r.json()[0]['data']['user']['clips']['pageInfo']['hasNextPage']:
         doneParsing = True
         print("[SUCCESS] Done. Fetched {} clips".format(currentCount))
