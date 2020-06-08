c06e113d7caacad11ffb220b0b77b542fa7aebdf
Torben <torben.capiau@accenture.com>
2020-06-08 20:05:25 +0200

    done.txt keeps track of already downloaded files

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ import requests, argparse, os, unicodedata
 from slugify import slugify
 
 download_path = "./downloads"
+filesList = []
+
+def MarkDone(slug):
+    with open("done.txt", "a+") as myfile:
+        myfile.write("{}\n".format(slug))
+
+def AlreadyDownloaded(slug):
+    with open('done.txt') as myfile:
+        if slug in myfile.read():
+            return True
+    return False
 
 def GetCuratorId(username, auth):
     data = [{"operationName":"ClipsManagerTable_User","variables":{"login":username,"limit":20,"criteria":{"sort":"CREATED_AT_DESC","period":"ALL_TIME","curatorID":"0"},"cursor":None},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"b300f79444fdcf2a1a76c101f466c8c9d7bee49b643a4d7878310a4e03944232"}}}]
@@ -9,29 +20,33 @@ def GetCuratorId(username, auth):
     return r.json()[0]['data']['user']['id']
 
 def DownloadClip(clip, filename, i):
-    filename = "{}-{}.mp4".format(str(i), slugify(filename))
-    if "AT-cm" not in clip['node']['thumbnailURL']:
-        if "offset" in clip['node']['thumbnailURL'] or "index" in clip['node']['thumbnailURL']:
-            video_id = clip['node']['thumbnailURL'].split('/')[3].split('-')[0]
-            offset = clip['node']['thumbnailURL'].split('/')[3].split('-')[2]
-            if len(clip['node']['thumbnailURL'].split('/')[3].split('-')) > 5:
-                if "vod" in clip['node']['thumbnailURL'].split('/')[3]:
-                    clip_url = "https://{}/vod-{}-offset-{}.mp4".format(clip['node']['thumbnailURL'].split('/')[2], clip['node']['thumbnailURL'].split('/')[3].split('-')[1], clip['node']['thumbnailURL'].split('/')[3].split('-')[3])
+    if not AlreadyDownloaded(clip['node']['slug']):
+        filename = "{}-{}.mp4".format(str(i), slugify(filename))
+        if "AT-cm" not in clip['node']['thumbnailURL']:
+            if "offset" in clip['node']['thumbnailURL'] or "index" in clip['node']['thumbnailURL']:
+                video_id = clip['node']['thumbnailURL'].split('/')[3].split('-')[0]
+                offset = clip['node']['thumbnailURL'].split('/')[3].split('-')[2]
+                if len(clip['node']['thumbnailURL'].split('/')[3].split('-')) > 5:
+                    if "vod" in clip['node']['thumbnailURL'].split('/')[3]:
+                        clip_url = "https://{}/vod-{}-offset-{}.mp4".format(clip['node']['thumbnailURL'].split('/')[2], clip['node']['thumbnailURL'].split('/')[3].split('-')[1], clip['node']['thumbnailURL'].split('/')[3].split('-')[3])
+                    else:
+                        clip_url = "https://{}/{}-offset-{}-{}.mp4".format(clip['node']['thumbnailURL'].split('/')[2], str(video_id), clip['node']['thumbnailURL'].split('/')[3].split('-')[2], clip['node']['thumbnailURL'].split('/')[3].split('-')[3])
                 else:
-                    clip_url = "https://{}/{}-offset-{}-{}.mp4".format(clip['node']['thumbnailURL'].split('/')[2], str(video_id), clip['node']['thumbnailURL'].split('/')[3].split('-')[2], clip['node']['thumbnailURL'].split('/')[3].split('-')[3])
+                    clip_url = "https://{}/{}-offset-{}.mp4".format(clip['node']['thumbnailURL'].split('/')[2], str(video_id), str(offset))
             else:
-                clip_url = "https://{}/{}-offset-{}.mp4".format(clip['node']['thumbnailURL'].split('/')[2], str(video_id), str(offset))
+                clip_url = "https://{}/{}.mp4".format(clip['node']['thumbnailURL'].split('/')[2], clip['node']['thumbnailURL'].split('/')[3].split('-')[0])
         else:
-            clip_url = "https://{}/{}.mp4".format(clip['node']['thumbnailURL'].split('/')[2], clip['node']['thumbnailURL'].split('/')[3].split('-')[0])
+            if "AT-cm" in clip['node']['thumbnailURL']:
+                clip_url = "https://{}/AT-{}.mp4".format(clip['node']['thumbnailURL'].split('/')[2], clip['node']['thumbnailURL'].split('/')[3].split('-')[1])
+        if "-index-" in clip['node']['thumbnailURL']:
+            clip_url = clip_url.replace("-offset-", "-index-")
+        r = requests.get(clip_url)
+        with open("{}/{}".format(download_path, filename), 'wb') as f:
+            f.write(r.content)
+        MarkDone(clip['node']['slug'])
+        print("[SUCCESS] Saved as {}".format(filename))
     else:
-        if "AT-cm" in clip['node']['thumbnailURL']:
-            clip_url = "https://{}/AT-{}.mp4".format(clip['node']['thumbnailURL'].split('/')[2], clip['node']['thumbnailURL'].split('/')[3].split('-')[1])
-    if "-index-" in clip['node']['thumbnailURL']:
-        clip_url = clip_url.replace("-offset-", "-index-")
-    r = requests.get(clip_url)
-    with open("{}/{}".format(download_path, filename), 'wb') as f:
-        f.write(r.content)
-    print("[SUCCESS] Saved as {}".format(filename))
+        print("[SKIPPED] Already downloaded this file ({})".format(filename))
 
 # Create the parser
 my_parser = argparse.ArgumentParser(description='Downloads your Twitch clips')
@@ -62,7 +77,7 @@ i = 0
 
 print("[SUCCESS] Authenticated, curator ID: {}".format(curatorId))
 data = [{"operationName":"ClipsManagerTable_User","variables":{"login":Twitch_Username,"limit":limit_increase,"criteria":{"sort":"CREATED_AT_DESC","period":"ALL_TIME","curatorID":curatorId},"cursor":cursor},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"b300f79444fdcf2a1a76c101f466c8c9d7bee49b643a4d7878310a4e03944232"}}}]
-
+MarkDone("")
 while not doneParsing:
     r = requests.post("https://gql.twitch.tv/gql", headers={"Authorization": "OAuth {}".format(Twitch_Auth), "Client-Id": "kimne78kx3ncx6brgo4mv6wki5h1ko"}, json=data)
     currentCount = currentCount + limit_increase
